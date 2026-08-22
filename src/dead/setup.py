0378f3cac36d0a29e255cbf76ed8b90f01418774
Navya Jain <navvu.jain@gmail.com>
2026-08-22 17:36:19 +0530

    package for open loop control

--- src/dead/setup.py
+++ src/dead/setup.py
@@ -21,8 +21,8 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='yash',
-    maintainer_email='grandytsho@gmail.com',
+    maintainer='navya',
+    maintainer_email='navvu.jain@gmail.com',
     description='TODO: Package description',
     license='Apache-2.0',
     extras_require={
